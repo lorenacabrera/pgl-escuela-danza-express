9ffd063b4dff8eca123dca537554f2d41bd54c85
Maria Lorena <marialorenacg@gmail.com>
2024-12-16 19:27:41 +0000

    corrige script sql ddl

--- db/ddl-pgl-estudio-danza.sql
+++ db/ddl-pgl-estudio-danza.sql
@@ -1,5 +1,9 @@
-CREATE TABLE Alumno (
-    id_alumno INT NOT NULL AUTO_INCREMENT ,
+START TRANSACTION;
+
+DROP TABLE IF EXISTS alumno;
+
+CREATE TABLE alumno (
+    id_alumno INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     fecha_nacimiento DATE NOT NULL,
@@ -12,7 +16,9 @@ CREATE TABLE Alumno (
     CONSTRAINT ak_alumno_mail UNIQUE (mail)
 );
 
-CREATE TABLE Profesor (
+DROP TABLE IF EXISTS profesor;
+
+CREATE TABLE profesor (
     id_profesor INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
@@ -22,72 +28,88 @@ CREATE TABLE Profesor (
     CONSTRAINT pk_profesor PRIMARY KEY (id_profesor)
 );
 
-CREATE TABLE Clase (
-    id_clase INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL,
-    descripcion VARCHAR(250) NULL,
-    nivel ENUM('principiante', 'intermedio', 'avanzado') NOT NULL,
-    dias_horarios VARCHAR(100) NULL,
-    max_estudiantes INT NOT NULL,
-    id_profesor INT NOT NULL,
-    CONSTRAINT pk_clase PRIMARY KEY (id_clase),
-    CONSTRAINT fk_clase_profesor FOREIGN KEY (id_profesor) REFERENCES   Profesor(id_profesor)
-);
+DROP TABLE IF EXISTS clase;
+-- 
+-- CREATE TABLE clase (
+--     id_clase INT NOT NULL AUTO_INCREMENT,
+--     nombre VARCHAR(50) NOT NULL,
+--     descripcion VARCHAR(250) NULL,
+--     nivel ENUM('principiante', 'intermedio', 'avanzado') NOT NULL,
+--     dias_horarios VARCHAR(100) NULL,
+--     max_estudiantes INT NOT NULL,
+--     id_profesor INT NOT NULL,
+--     CONSTRAINT pk_clase PRIMARY KEY (id_clase),
+--     CONSTRAINT fk_clase_profesor FOREIGN KEY (id_profesor) REFERENCES   profesor(id_profesor)
+-- );
+-- 
+-- DROP TABLE IF EXISTS inscripcion;
+-- 
+-- CREATE TABLE inscripcion (
+--     id_alumno INT NOT NULL,
+--     id_clase INT NOT NULL,
+--     asistencia VARCHAR(5) NULL,
+--     fecha_inscripcion DATE NOT NULL,
+--     CONSTRAINT pk_inscripcion PRIMARY KEY (id_alumno, id_clase), 
+--     CONSTRAINT fk_inscripcion_alumno FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno), 
+--     CONSTRAINT fk_inscripcion_clase FOREIGN KEY (id_clase) REFERENCES Clase(id_clase)  
+-- );
+-- 
+-- DROP TABLE IF EXISTS pago;
+-- 
+-- CREATE TABLE pago (
+--     id_pago INT NOT NULL AUTO_INCREMENT,
+--     id_alumno INT NOT NULL,
+--     fecha_pago DATE NOT NULL,
+--     monto DECIMAL(10, 2) NOT NULL,
+--     metodo_pago ENUM('tarjeta', 'transferencia', 'efectivo') NOT NULL,
+--     estado ENUM('pagado', 'pendiente', 'mora') NOT NULL,
+--     concepto VARCHAR(100) NOT NULL,
+--     CONSTRAINT pk_pago PRIMARY KEY (id_pago),
+--     CONSTRAINT chk_pago_monto CHECK (monto > 0), 
+--     CONSTRAINT fk_pago_alumno FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno)
+-- );
+-- 
+-- DROP TABLE IF EXISTS red_social;
+-- 
+-- CREATE TABLE red_social (
+--     id_redsocial INT NOT NULL AUTO_INCREMENT,
+--     plataforma VARCHAR(50) NOT NULL,
+--     usuario VARCHAR(100) NOT NULL,
+--     descripcion VARCHAR(255),
+--     CONSTRAINT pk_redsocial PRIMARY KEY (id_redsocial)
+-- );
+-- 
+-- DROP TABLE IF EXISTS evento;
+-- 
+-- CREATE TABLE evento (
+--     id_evento INT NOT NULL AUTO_INCREMENT,
+--     nombre_evento VARCHAR(100) NOT NULL,
+--     fecha DATE NOT NULL,
+--     ubicacion VARCHAR(255) NULL,
+--     descripcion VARCHAR(255) NULL,
+--     id_redsocial INT,
+--     CONSTRAINT pk_evento PRIMARY KEY (id_evento),
+--     CONSTRAINT fk_evento_redsocial FOREIGN KEY (id_redsocial) REFERENCES red_social(id_redsocial)
+-- );
+-- 
+-- DROP TABLE IF EXISTS participacion;
+-- 
+-- CREATE TABLE participacion (
+--     id_alumno INT NOT NULL,
+--     id_evento INT NOT NULL,
+--     CONSTRAINT pk_participacion PRIMARY KEY (id_alumno, id_evento),
+--     CONSTRAINT fk_participacion_alumno FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno),
+--     CONSTRAINT fk_participacion_evento FOREIGN KEY (id_evento) REFERENCES evento(id_evento)
+-- );
+-- 
+-- DROP TABLE IF EXISTS coordinacion;
+-- 
+-- CREATE TABLE coordinacion (
+--     id_profesor INT NOT NULL,
+--     id_evento INT NOT NULL,
+--     CONSTRAINT pk_coordinacion PRIMARY KEY (id_profesor, id_evento),
+--     CONSTRAINT fk_coordinacion_profesor FOREIGN KEY (id_profesor) REFERENCES profesor(id_profesor),
+--     CONSTRAINT fk_coordinacion_evento FOREIGN KEY (id_evento) REFERENCES evento(id_evento)
+-- );
 
-CREATE TABLE Inscripcion (
-    id_alumno INT NOT NULL,
-    id_clase INT NOT NULL,
-    asistencia VARCHAR(5) NULL,
-    fecha_inscripcion DATE NOT NULL,
-    CONSTRAINT pk_inscripcion PRIMARY KEY (id_alumno, id_clase), 
-    CONSTRAINT fk_inscripcion_alumno FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno), 
-    CONSTRAINT fk_inscripcion_clase FOREIGN KEY (id_clase) REFERENCES Clase(id_clase)  
-);
-
-CREATE TABLE Pago (
-    id_pago INT NOT NULL AUTO_INCREMENT,
-    id_alumno INT NOT NULL,
-    fecha_pago DATE NOT NULL,
-    monto DECIMAL(10, 2) NOT NULL,
-    metodo_pago ENUM('tarjeta', 'transferencia', 'efectivo') NOT NULL,
-    estado ENUM('pagado', 'pendiente', 'mora') NOT NULL,
-    concepto VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_pago PRIMARY KEY (id_pago),
-    CONSTRAINT chk_pago_monto CHECK (monto > 0), 
-    CONSTRAINT fk_pago_alumno FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno)
-);
-
-CREATE TABLE RedSocial (
-    id_redsocial INT NOT NULL AUTO_INCREMENT,
-    plataforma VARCHAR(50) NOT NULL,
-    usuario VARCHAR(100) NOT NULL,
-    descripcion VARCHAR(255),
-    CONSTRAINT pk_redsocial PRIMARY KEY (id_redsocial)
-);
-
-CREATE TABLE Evento (
-    id_evento INT NOT NULL AUTO_INCREMENT,
-    nombre_evento VARCHAR(100) NOT NULL,
-    fecha DATE NOT NULL,
-    ubicacion VARCHAR(255) NULL,
-    descripcion VARCHAR(255) NULL,
-    id_redsocial INT,
-    CONSTRAINT pk_evento PRIMARY KEY (id_evento),
-    CONSTRAINT fk_evento_redsocial FOREIGN KEY (id_redsocial) REFERENCES     RedSocial(id_redsocial)
-);
-
-CREATE TABLE Participacion (
-    id_alumno INT NOT NULL,
-    id_evento INT NOT NULL,
-    CONSTRAINT pk_participacion PRIMARY KEY (id_alumno, id_evento),
-    CONSTRAINT fk_participacion_alumno FOREIGN KEY (id_alumno) REFERENCES Alumno(id_alumno),
-    CONSTRAINT fk_participacion_evento FOREIGN KEY (id_evento) REFERENCES Evento(id_evento)
-);
-
-CREATE TABLE Coordinacion (
-    id_profesor INT NOT NULL,
-    id_evento INT NOT NULL,
-    CONSTRAINT pk_coordinacion PRIMARY KEY (id_profesor, id_evento),
-    CONSTRAINT fk_coordinacion_profesor FOREIGN KEY (id_profesor) REFERENCES Profesor(id_profesor),
-    CONSTRAINT fk_coordinacion_evento FOREIGN KEY (id_evento) REFERENCES Evento(id_evento)
-);
+COMMIT;
