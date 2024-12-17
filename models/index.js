74da722d24268850e8fdf710cdb1c9b0a2cd275d
Maria Lorena <marialorenacg@gmail.com>
2024-12-17 19:50:54 +0000

    sigue estructura propuesta por Sequelize CLI

--- models/index.js
+++ models/index.js
@@ -1,27 +1,43 @@
-import { dbConfig } from "../config/db.config.js";
+'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
-    }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-// const db = {};
-// 
-// db.Sequelize = Sequelize; // Sequelize export
-// db.sequelize = sequelize; // Sequelize object
-// 
-// db.EIDG = require("./Alumna.model.js")(sequelize,Sequelize);
-// 
-// module.exports = db;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-export { sequelize }
-
+module.exports = db;
