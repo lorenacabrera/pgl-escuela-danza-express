e25913de471a821dc491ce503fbb4a0fd1d9c23f
Lorena Cabrera <marialorenacg@gmail.com>
2025-11-16 13:12:17 +0000

    modificando multer

--- multer/upload.js
+++ multer/upload.js
@@ -1,33 +1,20 @@
 const multer = require('multer');
 const path = require('path');
 
-// Configuración del almacenamiento de Multer (donde se guardarán los archivos subidos)
+// Configurar almacenamiento
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // Indica la carpeta donde se guardarán las imágenes
-    cb(null, 'public/images'); // Ruta donde se guardarán las imágenes
+    cb(null, 'public/images'); // carpeta donde se guardarán las imágenes
   },
   filename: (req, file, cb) => {
-    var filetype = '';
-    if(file.mimetype === 'image/gif'){
-        filetype = 'gif';
-    }
-    if(file.mimetype === 'image/png'){
-        filetype = 'png';
-    }
-    if(file.mimetype === 'image/jpeg'){
-        filetype = 'jpg';
-    }
-    cb(null, 'image-' + Date.now() + '_' + filetype);
+    // Generar un nombre único para el archivo
+    const ext = path.extname(file.originalname).toLowerCase();
+    const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9) + ext;
 
-    // Establecer un nombre único para el archivo subido
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname)); // Asigna un nombre único
+    cb(null, uniqueName); // SOLO UNA VEZ
   }
 });
 
-// Crear una instancia de Multer con la configuración de almacenamiento
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
-// Exportar el middleware de Multer para su uso en otros archivos
 module.exports = upload;
