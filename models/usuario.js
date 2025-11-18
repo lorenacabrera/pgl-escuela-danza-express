76ad3697e318def3dad5397467dda0b58665f800
Lorena Cabrera <marialorenacg@gmail.com>
2025-11-18 21:29:21 +0000

    añadir id en el modelo usuario

--- models/usuario.js
+++ models/usuario.js
@@ -1,16 +1,20 @@
 module.exports = (sequelize, Sequelize) => {
-    const Usuario = sequelize.define("usuario", {
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false
-      }
-    });
-  
-    return Usuario;
-  };
-  
+  const Usuario = sequelize.define("usuario", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  });
+
+  return Usuario;
+};
